77b04c078ccf72ea5d257ef5af1a0ef10b494ff2
jinhankim988 <jinhankim988@douzone.com>
2023-08-09 19:45:06 +0900

    Batch Test

--- batch-processing/src/test/java/com/example/batchprocessing/batch/BatchCapitalizeConfigurationTest.java
+++ batch-processing/src/test/java/com/example/batchprocessing/batch/BatchCapitalizeConfigurationTest.java
@@ -1,39 +1,30 @@
 package com.example.batchprocessing.batch;
 
-import com.example.batchprocessing.TestDataSourceConfiguration;
-import com.example.batchprocessing.batch.listener.ItemFailureLoggerListener;
-import com.example.batchprocessing.batch.listener.JobCompletionNotificationListener;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
-import javax.sql.DataSource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@SpringBootTest
 @SpringBatchTest
-@SpringJUnitConfig({
-        BatchCapitalizeConfiguration.class,
-        Config.class,
-        TestDataSourceConfiguration.class,
-        JobCompletionNotificationListener.class,
-        ItemFailureLoggerListener.class,
-})
-@EnableBatchProcessing
 public class BatchCapitalizeConfigurationTest {
     @Autowired
     private JobLauncherTestUtils jobLauncherTestUtils;
-    @Autowired
-    private DataSource dataSource;
 
     @Test
-    public void testJob(@Autowired @Qualifier("capitalizeJob") Job job) throws Exception {
-        this.jobLauncherTestUtils.setJob(job);
+    public void testJob(@Autowired
+                        @Qualifier("capitalizeJob") Job job) throws Exception {
+        jobLauncherTestUtils.setJob(job);
 
-//        JobExecution jobExecution = jobLauncherTestUtils.launchJob();
-//        assertEquals("COMPLETED", jobExecution.getExitStatus().getExitCode());
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob();
+        assertEquals("COMPLETED", jobExecution.getExitStatus().getExitCode());
     }
 }
